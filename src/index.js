dbd9ec19bd1ed4e422b1f05960b29ab091e480f7
Dante Avila <danteavila1@hotmail.com>
2023-12-13 18:08:54 -0300

    temporarily deleted the other .js files in the src dir. Now you can add to the page the name of a new project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import taskBoard from "./taskBoard";
-import newProject from "./newProject";
+/* import { addProjectFnc } from "./newProject"; */
 
-
-const task = document.querySelector('.task');
 const addProject = document.querySelector('.add-project-btn');
 
 addProject.addEventListener('click', () => {
     newProject();
 })
+
+
+const newProject = () => {
+    const projectForm = document.querySelector("#project-form");
+    projectForm.classList.remove("hidden");
+
+    const add = document.querySelector(".add-btn");
+    add.addEventListener("click", addProjectForm);
+
+    const cancel = document.querySelector(".cancel-btn");
+    cancel.addEventListener("click", hideProjectForm);
+}
+
+
+    const addProjectForm = () => {
+        const projectInput = document.getElementById("project-input").value;
+        const addProjectBtn = document.querySelector(".add-project-btn");
+        const projectName = document.createElement("div")
+        projectName.classList.add("project-name");
+        projectName.textContent = projectInput;
+        addProjectBtn.appendChild(projectName);
+    
+    }
+
+    const hideProjectForm = () => {
+        const projectForm = document.querySelector("#project-form");
+        const projectInput = document.querySelector('#project-input');
+
+        projectInput.value = "";
+        projectForm.classList.add("hidden");
+    }
+
+  
