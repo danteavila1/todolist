263b04737e415d952380bc4634a7d52122b8d125
Dante Avila <danteavila1@hotmail.com>
2023-12-13 23:17:39 -0300

    created a UI file, and now when added a project the form to the task will show up

---
+++ src/UI.js
@@ -0,0 +1,37 @@
+
+const newProject = () => {
+    const projectForm = document.querySelector("#project-form");
+    projectForm.classList.remove("hidden");
+
+    const add = document.querySelector(".add-btn");
+    add.addEventListener("click", addProjectForm);
+
+    const cancel = document.querySelector(".cancel-btn");
+    cancel.addEventListener("click", hideProjectForm);
+}
+
+
+const addProjectForm = () => {
+    const projectInput = document.getElementById("project-input").value;
+    const addProjectBtn = document.querySelector(".add-project-btn");
+    const projectName = document.createElement("div")
+    projectName.classList.add("project-name");
+    projectName.textContent = projectInput;
+    addProjectBtn.appendChild(projectName);
+    loadProjectBoard(projectInput);
+}
+
+const hideProjectForm = () => {
+    const projectForm = document.querySelector("#project-form");
+    const projectInput = document.querySelector('#project-input');
+    projectInput.value = "";
+    projectForm.classList.add("hidden");
+}
+
+const loadProjectBoard = (projectInput) => {
+    document.getElementById('edit-project-form').classList.remove("hidden");
+    const projectTitle = document.querySelector(".project-title");
+    projectTitle.textContent = projectInput;
+}
+
+export default newProject;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* import { addProjectFnc } from "./newProject"; */
+import newProject from "./UI";
 
 const addProject = document.querySelector('.add-project-btn');
 
@@ -7,34 +7,5 @@ addProject.addEventListener('click', () => {
 })
 
 
-const newProject = () => {
-    const projectForm = document.querySelector("#project-form");
-    projectForm.classList.remove("hidden");
-
-    const add = document.querySelector(".add-btn");
-    add.addEventListener("click", addProjectForm);
-
-    const cancel = document.querySelector(".cancel-btn");
-    cancel.addEventListener("click", hideProjectForm);
-}
-
-
-    const addProjectForm = () => {
-        const projectInput = document.getElementById("project-input").value;
-        const addProjectBtn = document.querySelector(".add-project-btn");
-        const projectName = document.createElement("div")
-        projectName.classList.add("project-name");
-        projectName.textContent = projectInput;
-        addProjectBtn.appendChild(projectName);
-    
-    }
-
-    const hideProjectForm = () => {
-        const projectForm = document.querySelector("#project-form");
-        const projectInput = document.querySelector('#project-input');
-
-        projectInput.value = "";
-        projectForm.classList.add("hidden");
-    }
 
   
